7bf82ebd0d58eec958160ee5186648fa286ac580
Juan David <juandavid918@gmail.com>
2023-05-28 11:52:50 -0500

    List  comprehensions checked

---
+++ 03-fp/09_comprehensions.py
@@ -0,0 +1,10 @@
+# list, set, dictionary
+my_list = [char for char in "hello"]
+my_list2 = [num for num in range(0, 100)]
+my_list3 = [num ** 2 for num in range(0, 100)]
+my_list4 = [num ** 2 for num in range(0, 100) if num % 2 == 0]
+
+# for char in "hello":
+#     my_list.append(char)
+    
+print(my_list4)
